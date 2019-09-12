d176c9af05ce51a33f5c7ceba966d9c333f94ba1
spliitzx <fsmitchm@gmail.com>
2019-09-12 13:40:06 +0800

    NO SEGFAULTSgit add .git add .

--- main.py
+++ main.py
@@ -2,5 +2,21 @@ import cv2
 import numpy as np
 from matplotlib import pyplot
 
-print("hello WORLD!")
+query = cv2.imread("1.png", 0)
+train = cv2.imread("2.png", 0)
+
+orb = cv2.ORB_create()
+print(query)
+# find key points
+x1, y1 = orb.detectAndCompute(query, None)
+x2, y2 = orb.detectAndCompute(train, None)
+
+# measure distances, verify accuracy
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
+
+keyf = sorted(bf.match(y1, y2), key = lambda x: x.distance)
+final = cv2.drawMatches(query, x1, train, x2, keyf[:10], None, flags=2)
+
+# draw matches
+pyplot.imshow(final),pyplot.show()
 
